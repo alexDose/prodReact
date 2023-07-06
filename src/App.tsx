78abbe14f0619ff94a08e9a3af5fafc9f3d3b9d9
Alexei <i0ver@mail.ru>
2023-07-06 16:46:34 +0300

    add react-router-dom, bundle/chunks(lazy)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Counter} from "./components/Counter";
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from './pages/MainPage/MainPage.async';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import { Suspense } from 'react';
 
 const App = () => {
     return (
         <div className={'app'}>
-            <Counter/>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
