d66df386b640564fe6be79fc5bee0a7fa719c5e8
Alexei <i0ver@mail.ru>
2023-07-11 18:19:44 +0300

    add sidebar, add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,24 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {UseTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar/ui";
+import {Suspense} from 'react';
 
+import "shared/config/i18n/i18n";
+import {LangSwitcher} from "widgets/LangSwitcher";
 
 const App = () => {
     const {theme} = UseTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
-            <AppRouter/>
+            <Suspense fallback=''>
+                <Navbar/>
+                <div className='container'>
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,19 +1,20 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import styles from './Navbar.module.scss'
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import {useTranslation} from "react-i18next";
 
 interface NavbarProps {
     className?: string
 }
 
 export const Navbar = ({className}: NavbarProps) => {
+    const {t} = useTranslation()
+
     return (
         <div className={classNames(styles.Navbar, {}, [className])}>
-            <ThemeSwitcher/>
             <div className={styles.mainLinks}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>Main</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>About</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>{t('Main')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>{t('About')}</AppLink>
             </div>
         </div>
     );
