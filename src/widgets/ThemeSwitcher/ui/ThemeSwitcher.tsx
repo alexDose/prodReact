e0352d5a053f1f800aea77f7e9ace06b500aacb8
Alexei <i0ver@mail.ru>
2023-07-11 09:29:20 +0300

    add svg on switcher theme

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,6 +2,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import styles from './ThemeSwitcher.module.scss'
 import {Theme, UseTheme} from "app/providers/ThemeProvider";
 import {Button, ThemeButton} from "shared/ui/Button/Button";
+import SwitcherIcon from 'shared/assets/icons/ThemeSwitcherIcon.svg'
 
 interface ThemeSwitcherProps {
     className?: string
@@ -12,7 +13,8 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
 
     return (<div>
             <Button theme={ThemeButton.CLEAR} className={classNames(styles.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
-                {theme === Theme.LIGHT ? 'LIGHT' : 'DARK'}
+                <SwitcherIcon width={50} height={30}/>
+                {/*{theme === Theme.LIGHT ? 'LIGHT' : 'DARK'}*/}
             </Button>
         </div>
 
