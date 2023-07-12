e5ad2d6f150523abc4a1def7e6a8cf9a767164a1
Alexei <i0ver@mail.ru>
2023-07-12 19:46:53 +0300

    add NotFoundPage, add LoaderPage

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import styles from './NotFoundPage.module.scss'
+import {useTranslation} from "react-i18next";
+
+interface NotFoundPageProps {
+    className?: string
+}
+
+export const NotFoundPage = ({className}: NotFoundPageProps) => {
+    const {t} = useTranslation('notFound')
+
+    return (
+        <div className={classNames(styles.NotFoundPage, {}, [className])}>
+            {t('Page not found')}
+        </div>
+    );
+};
