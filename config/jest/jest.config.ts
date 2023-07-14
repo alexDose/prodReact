282babdbfcde66d4d38b1ad3e49f44ef96b503c3
Alexei <i0ver@mail.ru>
2023-07-14 15:01:37 +0300

    add react testing library, add test for Button and Sidebar

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,6 +1,13 @@
 import type {Config} from 'jest';
+import path from "path";
 
 const config: Config = {
+    modulePaths: ["<rootDir>src"],
+    setupFilesAfterEnv: ['<rootDir>/config/jest/setupTest.ts'],
+    moduleNameMapper: {
+        '\\.(s?css)$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    },
     // All imported modules in your tests should be mocked automatically
     // automock: false,
 
