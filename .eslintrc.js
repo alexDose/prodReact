48d65db50dfdcaed71266d355044e98fa379869c
Alexei <i0ver@mail.ru>
2023-07-14 20:01:22 +0300

    Revert "add Storybook"
    This reverts commit 34b56226ceff6743a93c8c903cacd29d469575a8.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,54 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'jest': true
-  },
-  'extends': ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
-  'overrides': [{
     'env': {
-      'node': true
+        'browser': true,
+        'es2021': true,
+        'jest': true
     },
-    'files': ['.eslintrc.{js,cjs}'],
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:i18next/recommended'
+    ],
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parser': '@typescript-eslint/parser',
     'parserOptions': {
-      'sourceType': 'script'
-    }
-  }],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
-  },
-  'plugins': ['@typescript-eslint', 'react', 'i18next'],
-  'rules': {
-    'linebreak-style': ['error', 'unix'],
-    'semi': ['error', 'always'],
-    "@typescript-eslint/ban-ts-comment": "warn",
-    "no-unused-vars": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "indent": ["warn", 2],
-    "react/jsx-filename-extension": [1, {
-      "extensions": [".js", ".jsx", ".tsx"]
-    }],
-    "quotes": ["error", "single"],
-    "i18next/no-literal-string": ["error", {
-      markupOnly: true
-    }]
-  }
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint',
+        'react',
+        'i18next'
+    ],
+    'rules': {
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        "@typescript-eslint/ban-ts-comment": "warn",
+        "no-unused-vars": "warn",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "indent": ["warn", 2],
+        "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx", ".tsx"]}],
+        "quotes": ["error", "single"],
+        "i18next/no-literal-string": ["error",{markupOnly: true}]
+    },
 };
