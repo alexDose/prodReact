10a26e5012b1a423be8aa2b9a0e104f37a37f1f9
Alexei <i0ver@mail.ru>
2023-07-14 15:01:37 +0300

    add react testing library, add test for Button and Sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/SideBar.test.tsx
@@ -0,0 +1,18 @@
+import {fireEvent, screen} from "@testing-library/react";
+import {Sidebar} from "widgets/Sidebar/ui/Sidebar/Sidebar";
+import {renderWithTranslation} from "shared/lib/renderWithTranslation/renderWithTranslation";
+
+describe('sidebar', () => {
+    test('test', () => {
+        renderWithTranslation(<Sidebar/>)
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument()
+    })
+
+    test('test toggle', () => {
+        renderWithTranslation(<Sidebar/>)
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument()
+        const toggleBtn = screen.getByTestId('sidebar-toggle')
+        fireEvent.click(toggleBtn)
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
+    })
+})
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import styles from './Sidebar.module.scss';
 import {useState} from 'react';
 import {ThemeSwitcher} from 'widgets/ThemeSwitcher';
 import {LangSwitcher} from 'widgets/LangSwitcher';
+import {Button} from "../../../../shared/ui/Button/Button";
 
 interface SidebarProps {
     className?: string
@@ -17,8 +18,13 @@ export const Sidebar = ({className}: SidebarProps) => {
 	};
 
 	return (
-		<div className={classNames(styles.Sidebar, {[styles.collapsed]: collapsed}, [className])}>
-			<button onClick={onToggle}>toggle</button>
+		<div data-testid='sidebar' className={classNames(styles.Sidebar, {[styles.collapsed]: collapsed}, [className])}>
+			<Button
+				data-testid='sidebar-toggle'
+				onClick={onToggle}
+			>
+				toggle
+			</Button>
 			<div className={styles.switchers}>
 				<ThemeSwitcher/>
 				<LangSwitcher/>
