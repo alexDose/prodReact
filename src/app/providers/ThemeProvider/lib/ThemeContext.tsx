115d53018f5db90c9ec02db978095eb38538fd8c
alexDose <i0ver@mail.ru>
2025-12-11 20:29:00 +0300

    add new theme

--- src/app/providers/ThemeProvider/lib/ThemeContext.tsx
+++ src/app/providers/ThemeProvider/lib/ThemeContext.tsx
@@ -2,7 +2,8 @@ import {createContext} from 'react';
 
 export enum Theme {
     LIGHT = 'app_light_theme',
-    DARK = 'app_dark_theme'
+    DARK = 'app_dark_theme',
+    ROSE = 'app_rose_theme',
 }
 
 export interface ThemeContextProps {
